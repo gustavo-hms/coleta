b58305234aaa881d3bb53165c97b02ef36127040
Joao Ricardo Petreli Jorge <joao.petrelli@gmail.com>
2013-05-10 13:07:50 -0300

    Remaing zona methods.

--- dao/zona_dao.go
+++ dao/zona_dao.go
@@ -18,6 +18,50 @@ func NewZonaDAO(tx *sql.Tx) *ZonaDAO {
 	}
 }
 
+func (dao *ZonaDAO) Save(zona *modelos.Zona) error {
+	if zona.Id == 0 {
+		return dao.create(zona)
+	} else {
+		return dao.update(zona)
+	}
+}
+
+func (dao *ZonaDAO) create(zona *modelos.Zona) {
+	query := fmt.Sprintf("INSERT INTO zona (%s) VALUES (DEFAULT, ?)", dao.fields)
+	res, err := dao.Exec(query, zona.Nome)
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	zona.Id = id
+	return nil
+}
+
+func (dao *ZonaDAO) update(zona *modelos.Zona) {
+	query := "UPDATE zona SET nome = ?"
+	row, err := dao.Exec(query, zona.Nome)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := row.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRowsNotAffected
+	}
+
+	return nil
+
+}
+
 func (dao *ZonaDAO) FindAll() (zonas []*modelos.Zona, err error) {
 	query := fmt.Sprintf("SELECT %s FROM zona", dao.fields)
 	rows, err := dao.Query(query)
@@ -36,4 +80,24 @@ func (dao *ZonaDAO) FindAll() (zonas []*modelos.Zona, err error) {
 	}
 
 	return zonas, nil
+}
+
+func (dao *ZonaDAO) Delete(id int) error {
+	query := "DELETE FROM zona WHERE id = ?"
+	res, err := dao.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return ErrRowsNotAffected
+	}
+
+	return err
+
 }
