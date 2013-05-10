7664b2a4aa068a2227afc945f9cec2335ddea323
Joao Ricardo Petreli Jorge <joao.petrelli@gmail.com>
2013-05-10 11:09:30 -0300

    Database schema

---
+++ doc/schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE zona (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(255),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE esquina (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	zona_id INT(11) NOT NULL,
+	cruzamento VARCHAR(255),
+	localizacao VARCHAR(255),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE lider (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	zona_id INT(11) NOT NULL,
+	esquina_id INT(11) NOT NULL,
+	criado_em DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+	nome_completo VARCHAR(255),
+	telefone_residencial VARCHAR(20),
+	telefone_celular VARCHAR(20),
+	operadora_celular ENUM('CLARO', 'OI', 'OUTRA', 'TIM', 'VIVO'),
+	email VARCHAR(255),
+	turno VARCHAR(255),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE voluntarios (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	zona_id INT(11) NOT NULL,
+	lider_id INT(11) NOT NULL,
+	nome_completo VARCHAR(255),
+	telefone_residencial VARCHAR(20),
+	telefone_celular VARCHAR(20),
+	operadora_celular ENUM('CLARO', 'OI', 'OUTRA', 'TIM', 'VIVO'),
+	email VARCHAR(255),
+	turno VARCHAR(255),
+	como_soube_coleta_2013 VARCHAR(255),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB;
