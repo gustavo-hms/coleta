36624faa74be892c8f21334fd8b1004bef3d4b61
Gustavo Henrique Montesião de Sousa <gustavo@cecm.usp.br>
2013-06-11 21:26:57 -0300

    Lendo arquivo de configuração

--- servidor.go
+++ servidor.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"coleta/config"
+	"coleta/db"
 	_ "coleta/handlers"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println(uso())
+	}
+
+	if err := config.Ler(os.Args[1]); err != nil {
+		fmt.Printf("Não foi possível ler o arquivo de configuração %s. Erro: %s", os.Args[1], err)
+		os.Exit(1)
+	}
+
+	if err := db.Conn(); err != nil {
+		fmt.Println("Não foi possível conectar-se ao banco. Erro:", err)
+		os.Exit(1)
+	}
+
 	http.ListenAndServe(":8080", nil)
+}
+
+func uso() string {
+	return "Uso: " + os.Args[0] + " <arquivo de configuração>"
 }
