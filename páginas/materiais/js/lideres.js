1dad0e5544ad2dd2d19e5e5ac2c0b60b5d03400c
Gustavo Henrique Montesião de Sousa <gustavo@cecm.usp.br>
2013-07-02 20:24:07 -0300

    Resolvido problema do formulário cortado; melhor apresentação do diálogo de confirmação

--- páginas/materiais/js/lideres.js
+++ páginas/materiais/js/lideres.js
@@ -3,12 +3,19 @@ $(function() {
 });
 
 function confirmacao() {
+	var textoOriginal = $("#texto-turnos").text()
+
 	$('#enviar').click(function(e) {
 		var turnos = $.map($("#turnos > input:checked + label"), function(x) {
 			return $(x).text()
 		});
 
-		$("#confirmacao-turnos").html(turnos.join("<br/>"));
+		if (turnos.length == 0) {
+			$("#texto-turnos").text("Você não selecionou nenhum turno!");
+		} else {
+			$("#texto-turnos").text(textoOriginal)
+		}
+		$("#confirmacao-turnos").html(turnos.map(function(turno) {return "<li>" + turno + "</li>"}));
 		$("#confirmacao-zona").html($("#zona > :selected").html());
 
 		e.preventDefault();
@@ -16,7 +23,7 @@ function confirmacao() {
 			resizable: false,
 			dialogClass: "dialog",
 			height: 330,
-			width: 680,
+			width: 480,
 			modal: true,
 			title: "Confirmação",
 			buttons: {
