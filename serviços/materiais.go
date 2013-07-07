886f4f4cafd05875b174304528a0db9264982466
Gustavo Henrique Montesião de Sousa <gustavo@cecm.usp.br>
2013-07-07 17:10:15 -0300

    Adicionada rota para os materiais em /adm/

--- serviços/materiais.go
+++ serviços/materiais.go
@@ -3,14 +3,17 @@ package serviços
 import (
 	"coleta/config"
 	"net/http"
+	"strings"
 )
 
 func init() {
 	registrar("/materiais/", Materiais{})
+	registrarSeguro("/adm/materiais/", Materiais{})
 }
 
 type Materiais struct{}
 
 func (m Materiais) Get(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, config.Dados.DiretórioDasPáginas+r.URL.String())
+	path := strings.TrimPrefix(r.URL.String(), "/adm")
+	http.ServeFile(w, r, config.Dados.DiretórioDasPáginas+path)
 }
