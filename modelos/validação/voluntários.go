1de1453ddd428ab387bc87d511343ed7dbe12eea
Gustavo Henrique Montesião de Sousa <gustavo@cecm.usp.br>
2013-07-10 20:00:49 -0300

    Faltaram alguns arquivos

---
+++ modelos/validação/voluntários.go
@@ -0,0 +1,117 @@
+package validação
+
+import (
+	"coleta/dao"
+	"coleta/modelos"
+	"log"
+	"net/mail"
+	"strings"
+)
+
+type VoluntárioComErros struct {
+	modelos.Voluntário
+
+	errosEncontrados bool
+
+	MsgNome                string
+	MsgTelefoneResidencial string
+	MsgTelefoneCelular     string
+	MsgOperadora           string
+	MsgEmail               string
+	MsgContato             string
+	MsgTurnos              string
+	MsgZona                string
+	MsgCadastradoEm        string
+	MsgEsquina             string
+	MsgRG                  string
+	MsgCPF                 string
+}
+
+func NovoVoluntárioComErros(líder *modelos.Voluntário) *VoluntárioComErros {
+	v := new(VoluntárioComErros)
+	v.Voluntário = *líder
+
+	return v
+}
+
+func ValidarVoluntário(líder *modelos.Voluntário) *VoluntárioComErros {
+	return NovoVoluntárioComErros(líder).
+		validarCamposObrigatórios().
+		validarSintaxe().
+		validarPolíticas().
+		apurarErros()
+}
+
+func (v *VoluntárioComErros) apurarErros() *VoluntárioComErros {
+	if v.errosEncontrados {
+		return v
+	}
+
+	return nil
+}
+
+func (v *VoluntárioComErros) validarCamposObrigatórios() *VoluntárioComErros {
+	if v.Nome == "" {
+		v.errosEncontrados = true
+		v.MsgNome = "Este campo não pode estar vazio"
+	}
+
+	if v.TelefoneResidencial == "" && v.TelefoneCelular == "" {
+		v.errosEncontrados = true
+		v.MsgContato = "Ao menos um destes campos não pode estar vazio"
+	}
+
+	if v.Email == "" {
+		v.errosEncontrados = true
+		v.MsgEmail = "Este campo não pode estar vazio"
+	}
+
+	if len(v.Turnos) == 0 {
+		v.errosEncontrados = true
+		v.MsgTurnos = "Ao menos um turno tem de estar selecionado"
+	}
+
+	return v
+}
+
+func (v *VoluntárioComErros) validarSintaxe() *VoluntárioComErros {
+	if v.Email == "" {
+		return v
+	}
+
+	if _, err := mail.ParseAddress(v.Email); err != nil {
+		v.errosEncontrados = true
+		v.MsgEmail = "Este não é um e-mail válido"
+	}
+
+	return v
+}
+
+func (v *VoluntárioComErros) validarPolíticas() *VoluntárioComErros {
+	if v.Nome != "" && len(strings.Fields(v.Nome)) < 2 {
+		v.errosEncontrados = true
+		v.MsgNome = "Por favor, informe seu nome completo"
+	}
+
+	if v.Email == "" {
+		return v
+	}
+
+	if v.Id == 0 {
+		tx, err := dao.DB.Begin()
+		defer tx.Commit()
+		if err != nil {
+			log.Println(err)
+			return v
+		}
+
+		líderDAO := dao.NewLiderDAO(tx)
+
+		if mesmoEmail, _ := líderDAO.FindByEmail(v.Email); mesmoEmail != nil {
+			v.errosEncontrados = true
+			v.MsgEmail = "Já existe alguém cadastrado com este mesmo e-mail. Por favor, informe outro endereço. Em caso de dúvidas, contacte seu líder de zona"
+		}
+	}
+
+	return v
+}
---
+++ serviços/busca-líderes.go
@@ -0,0 +1,47 @@
+package serviços
+
+import (
+	"coleta/dao"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func init() {
+	registrar("/busca/lideres", BuscaLíderes{})
+}
+
+type BuscaLíderes struct{}
+
+func (b BuscaLíderes) Get(w http.ResponseWriter, r *http.Request) {
+	trecho := r.FormValue("contem")
+	if len(trecho) < 3 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	tx, err := dao.DB.Begin()
+	if err != nil {
+		log.Println("Início da transação:", err)
+		erroInterno(w, r)
+		return
+	}
+
+	líderDAO := dao.NewLiderDAO(tx)
+	líderes, err := líderDAO.FindAllThatMatches(trecho)
+	if err != nil {
+		log.Println(err)
+		erroInterno(w, r)
+		return
+	}
+
+	bytes, err := json.Marshal(líderes)
+	if err != nil {
+		log.Println(err)
+		erroInterno(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(bytes))
+}
